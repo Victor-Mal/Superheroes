25455046806e4964e4aef4a54faf4c697fa6bc4f
Yevhenii <freshjs.fte@gmail.com>
2021-11-16 19:57:10 +0200

    Add supheroes controllers and routes

--- routes/superheroes.js
+++ routes/superheroes.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const shController = require("../controllers/superheroes");
 
 /* Superheroes Router */
 const shRouter = express.Router();
 
 /* /api/superheroes */
 
-/* get, post, (put, patch), delete */
+/* HTTP CRUD methods: get, post, (put, patch), delete */
 
+shRouter
+  .route("/")
+  /* get all */
+  .get(shController.getAllSupHeroes)
+  /* create */
+  .post(shController.createSupHero);
 
+shRouter
+  .route("/:id")
+  /* get one */
+  .get(shController.getSupHeroById)
+  /* update */
+  .patch(shController.updateSupHero)
+  /* delete */
+  .delete(shController.deleteSupHero);
 
 module.exports = shRouter;
