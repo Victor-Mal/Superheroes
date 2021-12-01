67ee41a00f243ed019ba2220bf582be42213ebc8
Victor Mal <VictorianusVelikyu@gmail.com>
2021-12-01 04:16:22 +0200

    Remove comment from sup.heroes

--- routes/superheroes.js
+++ routes/superheroes.js
@@ -1,28 +1,17 @@
 const express = require("express");
 const shController = require("../controllers/superheroes");
-const validateHero = require("../middlewares/validateHero");
 
-/* Superheroes Router */
 const shRouter = express.Router();
-
-/* /api/superheroes */
-
-/* HTTP CRUD methods: get, post, (put, patch), delete */
 
 shRouter
   .route("/")
-  /* get all */
   .get(shController.getAllSupHeroes)
-  /* create */
-  .post(validateHero, shController.createSupHero);
+  .post(shController.createSupHero);
 
 shRouter
   .route("/:id")
-  /* get one */
   .get(shController.getSupHeroById)
-  /* update */
-  .patch(validateHero, shController.updateSupHero)
-  /* delete */
+  .patch(shController.updateSupHero)
   .delete(shController.deleteSupHero);
 
 module.exports = shRouter;
